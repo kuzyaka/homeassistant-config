8c16100a9e6f11a750b93b4d75ccf37537b69b56
kuzyaka <stupidpost@yandex.ru>
2020-01-20 20:31:16 +0400

    phone tracking, TG health, yandex notifier

---
+++ scripts/telegram_health.sh
@@ -0,0 +1,6 @@
+tg_token=$(grep -E 'telegram_bot_api_key\:' ../secrets.yaml | awk '{print $2}')
+if curl --silent --fail -m 60 -o /dev/null -G  https://api.telegram.org/bot$tg_token/getMe; then
+    echo online
+else
+    echo offline
+fi

a0e8ed6ea017e58ebddafb973ff920da63abbec5
kuzyaka <stupidpost@yandex.ru>
2020-02-02 21:32:52 +0400

    Use zigbee2mqtt on CC2511 instead of Xiaomi Gateway. Fix tekegram health check status

--- scripts/telegram_health.sh
+++ scripts/telegram_health.sh
@@ -1,5 +1,5 @@
 tg_token=$(grep -E 'telegram_bot_api_key\:' ../secrets.yaml | awk '{print $2}')
-if curl --silent --fail -m 60 -o /dev/null -G  https://api.telegram.org/bot$tg_token/getMe; then
+if curl --silent --fail -m 30 -o /dev/null -G  https://api.telegram.org/bot$tg_token/getMe; then
     echo online
 else
     echo offline
